637caf7895afdefc64923496e5899a0423ae3b6e
Pratik Chauhan <pratik.chauhan@vtnetzwelt.com>
2024-08-13 18:45:22 +0530

    Added environment variables with config file

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,20 +1,28 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
+
+// Load environment variables
+require('dotenv').config();
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `scubed-landing-page`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: process.env.siteURL || "https://default-url.com",
   },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
     "gatsby-plugin-vanilla-extract",
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: `gatsby-plugin-s3`,
       options: {
-        policy: [{userAgent: '*', disallow: '/'}]
-      }
-    }
+        bucketName: process.env.bucketName || "default-bucket-name",
+        protocol: "https",
+        hostname: process.env.hostname || "default-hostname.com",
+      },
+    },
   ],
-}
+};
 
-export default config
+export default config;
