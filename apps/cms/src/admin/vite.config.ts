5485fd618053ffb8f382ccdf020e66a5aaf21a53
Baljeet Singh <baljeet.singh@vtnetzwelt.com>
2025-08-20 11:59:21 +0530

    SCM-3475 - chore: Update GitLab CI configuration and add Vite config for CMS
    - Added a manual trigger for the deploy_to_ec2 job in .gitlab-ci.yml to enhance deployment control.
    - Introduced a new Vite configuration file for the CMS application to manage server settings and module resolution.

--- apps/cms/src/admin/vite.config.ts
+++ apps/cms/src/admin/vite.config.ts
@@ -1,6 +1,6 @@
-import { mergeConfig } from 'vite';
+import { mergeConfig, type UserConfig } from 'vite';
 
-export default (config) => {
+export default (config: UserConfig) => {
   // Important: always return the modified config
   return mergeConfig(config, {
     server: {
