fdf76802fbab455acbc82ea6c61cd96443387be9
Baljeet Singh <baljeet.singh@vtnetzwelt.com>
2025-08-20 12:49:26 +0530

    chore: Update CORS configuration in Strapi middleware
    - Modified the CORS middleware configuration in middlewares.ts to enable CORS with specific allowed origins for development and production environments. This change enhances security and ensures proper access for the Next.js application.

--- apps/cms/config/middlewares.ts
+++ apps/cms/config/middlewares.ts
@@ -2,7 +2,21 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:1337',
+        'https://dev.scubed.io',
+        'https://www.dev.scubed.io',
+        'https://scubed.io',
+        'https://www.scubed.io'
+      ]
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
