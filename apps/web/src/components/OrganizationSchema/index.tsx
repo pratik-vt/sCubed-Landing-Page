a31df566fc415ae783a8d7439d5f09b2e5fa5fa8
Ankush Kumar <ankush.kumar@vtnetzwelt.com>
2025-10-15 14:40:17 +0530

    SCM-3877 -  revised organization-schema

--- apps/web/src/components/OrganizationSchema/index.tsx
+++ apps/web/src/components/OrganizationSchema/index.tsx
@@ -11,6 +11,21 @@ const OrganizationSchema: React.FC = () => {
     description:
       'S Cubed empowers providers to manage ABA, OT, PT, speech, and counseling with ease - streamlining scheduling, billing, documentation, and compliance in one platform.',
     foundingDate: '2024',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: '3000 Illinois Ave',
+      addressLocality: 'Killeen',
+      addressRegion: 'TX',
+      postalCode: '76543',
+      addressCountry: 'US',
+    },
+    aggregateRating: {
+      '@type': 'AggregateRating',
+      ratingValue: '5',
+      bestRating: '5',
+      worstRating: '5',
+      ratingCount: '10',
+    },
     contactPoint: {
       '@type': 'ContactPoint',
       telephone: '+1-254-434-4959',
